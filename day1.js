1404158073a9952b00fe7aa3fe34c4b24c82e972
nayan32006 <nayang389@gmail.com>
2026-07-28 18:06:33 +0530

    fet " add the  1st day of the JS "

---
+++ day1.js
@@ -0,0 +1,25 @@
+const product = {
+    tittle: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 290,
+};
+
+// 1 let variable ex: let a= 10
+//                       a= 2;
+//                      console.log(a)
+// 2 var variable ex:var a= 10
+//                       a= 2;
+//                       a= 6;
+//                      console.log(a)
+// 3 const variable  ex : cons pi = 3.14
+//                      console.log(a)
+
+
+// 4 data type  on top ex : like 
+// const product = {
+//     tittle: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 290,
+// };
